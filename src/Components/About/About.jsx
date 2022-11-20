dfde3f09aadb13f4d035ccef0cde0da7a76dff7d
JuliusCapl <julio93.cl@gmail.com>
2022-11-20 00:27:35 -0300

    .

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,13 +9,11 @@ const About = () => {
         <div className="about--text py-5 px-md-5">
           <h3 className="mb-4">A little about me...</h3>
           <p>
-            My name is Julio Capdevilla, I am a Chilean man new in the industry,
-            trying to lear every day something new.
-            doiasoidu aosid oiasdoisoid oaisiu douum ipsum dolor sit amet
-            consectetur, adipisicing els perspiciatis eius placeat tenetur magni
-            debitis impedit ea? Fugit, quae incidunt? Labore accusantium
-            nesciunt suscipit ut at aspernatur placeat architecto molestiae
-            optio reiciendis?
+          My name is Julio Capdevilla, I am a Chilean man new in the industry, trying to learn every day something new.
+          This is my first portfolio and, in this section, it should say some cool things about me but I am not cool.
+          I have some knowledge in technologies below, Javascript, NodeJS, MongoDB, PostgreSQL, ReactJS.
+          Soon you will be able to see in the section "Projects" what I am working on and if you have any comment you can write me whatever you want in the section "Contact".
+          By the way the guy in the picture is not me.
           </p>
         </div>
         <div className="about--img">
